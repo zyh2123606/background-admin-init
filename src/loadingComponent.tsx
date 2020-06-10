5d0e0d107a38516a41ebc705c2087a7e9e6a9fc0
zyh2123 <641261604@qq.com>
2020-06-10 09:45:21 +0800

    提交

---
+++ src/loadingComponent.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react'
+import { View } from 'cy-element'
+
+const LoadingComponent: FunctionComponent = () => {
+    return <View></View>
+}
+export default LoadingComponent
